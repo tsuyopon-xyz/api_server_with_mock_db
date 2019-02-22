898eb2e8d689130f5632997aef6c8ad51c27c337
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2019-02-22 19:00:37 +0900

    Created a test for the post todos api.

---
+++ test/app/api/todos/postTodos.test.js
@@ -0,0 +1,69 @@
+const assert = require('power-assert');
+const requestHelper = require('../../../helper/requestHelper');
+
+const getTodos = async () => {
+  const response = await requestHelper.request({
+    method: 'get',
+    endPoint: '/api/todos',
+    statusCode: 200
+  });
+
+  return response.body;
+};
+
+describe('test 「POST /api/todos」', () => {
+  it('titleを送らなかったら400エラーが返る', async () => {
+    const postData = {body: 'test body'};
+
+    const response = await requestHelper.request({
+      method: 'post',
+      endPoint: '/api/todos',
+      statusCode: 400
+    }).send(postData);
+
+    assert.deepEqual(response.body, {
+      message: 'titleは必須です'
+    });
+  });
+
+  it('bodyを送らなかったら400エラーが返る', async () => {
+    const postData = {title: 'test title'};
+
+    const response = await requestHelper.request({
+      method: 'post',
+      endPoint: '/api/todos',
+      statusCode: 400
+    }).send(postData);
+
+    assert.deepEqual(response.body, {
+      message: 'bodyは必須です'
+    });
+  });
+
+  it('title, bodyを送ったら成功する', async () => {
+    const oldTodos = await getTodos();
+
+    const postData = {
+      title: 'test title',
+      body: 'test body'
+    };
+
+    const response = await requestHelper.request({
+      method: 'post',
+      endPoint: '/api/todos',
+      statusCode: 200
+    }).send(postData);
+
+    const createdTodo = response.body;
+    assert.deepEqual(createdTodo, {
+      id: createdTodo.id,
+      title: postData.title,
+      body: postData.body,
+      createdAt: createdTodo.createdAt,
+      updatedAt: createdTodo.updatedAt
+    });
+
+    const currentTodos = await getTodos();
+    assert.equal(oldTodos.length + 1, currentTodos.length);
+  });
+});
